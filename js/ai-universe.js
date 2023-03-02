d216e6a7bfe720971122adfd4afd2c3570a9f33b
bossnoyon00 <bossnoyon584@gmail.com>
2023-03-02 22:09:40 +0600

    modal commit

--- js/ai-universe.js
+++ js/ai-universe.js
@@ -7,7 +7,7 @@ const loadData = () => {
 const displayData = (datas) => {
     const container = document.getElementById('data-load');
     for (const data of datas) {
-        console.log(data);
+        // console.log(data);
         const newCard = document.createElement('div');
         newCard.classList.add('col')
         newCard.innerHTML = `
@@ -26,11 +26,39 @@ const displayData = (datas) => {
         <h5>${data.name}</h5>
         <h6><i class="fa-solid fa-calendar-days"></i> ${data.published_in}</h6>
         </div>
-        <div><i class="fa-solid fa-arrow-right"></i></div>
+        <div><i class="fa-solid fa-arrow-right" onclick="modalLoad('${data.id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></div>
         </div>
     </div>
         `;
         container.appendChild(newCard);
     }
 }
+
+const modalLoad = (id) => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showModalData(data.data))
+}
+
+const showModalData = (details) => {
+    console.log(details.description);
+
+    const container = document.getElementById('modal-load');
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('col');
+    newDiv.innerHTML = `
+    <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="">${details.description}</p>
+    </div>
+</div>
+    `;
+    container.appendChild(newDiv);
+
+}
+
 loadData();
